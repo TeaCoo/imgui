fd5bf0d0d8d6e6ee5153c829f217f99318397af6
TeaCoo <54407283+TeaCoo@users.noreply.github.com>
2022-12-31 02:51:03 +0800

    update premake

--- premake5.lua
+++ premake5.lua
@@ -18,7 +18,17 @@ project "ImGui"
     "imgui_tables.cpp",
     "imstb_truetype.h",
     "imgui_demo.cpp",
+	"./backends/imgui_impl_opengl3.h",
+	"./backends/imgui_impl_opengl3.cpp",
+	"./backends/imgui_impl_glfw.h",
+	"./backends/imgui_impl_glfw.cpp",
   }
+
+	includedirs
+	{
+		"./",
+		"./examples/libs/glfw/include/"
+	}
 
   filter "system:windows"
     systemversion "latest"
